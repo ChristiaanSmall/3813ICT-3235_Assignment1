9047f47fdfcc4f255c9646c46c6c93a5c91ba309
Christiaan <small.christiaan@gmail.com>
2023-09-04 18:47:34 +1000

    Updated Routing for all pages
    Added routing for login, admin-dashboard, group-admin-dashboard, user-dashboard, chat-window

--- 3813ICT-Chat/src/app/app-routing.module.ts
+++ 3813ICT-Chat/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { GroupAdminDashboardComponent } from './group-admin-dashboard/group-admin-dashboard.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'admin-dashboard', component: AdminDashboardComponent },
+  { path: 'group-admin-dashboard', component: GroupAdminDashboardComponent },
+  { path: 'user-dashboard', component: UserDashboardComponent },
+  { path: 'chat-window', component: ChatWindowComponent },
+  { path: '**', redirectTo: 'login' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
